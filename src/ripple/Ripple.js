ec923517395dc78b23e980b2ccab5cfa2bcd1117
박찬열 <ryury@sk.com>
2017-09-19 13:04:38 +0900

    modify ripple.js

--- src/ripple/Ripple.js
+++ src/ripple/Ripple.js
@@ -1,6 +1,7 @@
+/* Material Design Ripple effect in pure Javascript & CSS3 by SION. */
 class Ripple {
     constructor(el = '[data-ripple]', options = {}) {
-        this.ripples = (typeof el === 'string') ? document.querySelectorAll(el) : el;
+        this.ripples = Array.from((typeof el === 'string') ? document.querySelectorAll(el) : el);
         this.opts = Object.assign({}, this.constructor.defaults(), options);
 
         this.initialize();
@@ -34,23 +35,28 @@ class Ripple {
     }
 
     addHandler() {
-        Array.from(this.ripples).forEach((wrap) => {
-            wrap.addEventListener(this.opts.eventType, this.createRipple.bind(this), false);
+        this.ripples.forEach((wrap) => {
+            wrap.addEventListener(this.opts.eventType, this.rippleStart.bind(this), false);
         });
     }
 
-    createRipple(e) {
+    rippleStart(e) {
         e.stopPropagation();
 
-        const target = e.currentTarget.closest(`[${this.opts.selector}]`);
-        const {width: w, height: h} = target.getBoundingClientRect();
-        const x = e.clientX - target.offsetLeft;
-        const y = e.clientY - target.offsetTop;
-        const targetX = (x > w >> 1) ? 0 : w;
-        const targetY = (y > h >> 1) ? 0 : h;
-        const d = Math.sqrt(Math.pow(Math.abs(targetX - x), 2) + Math.pow(Math.abs(targetY - y), 2));
-        const targetStyle = target.style;
-        const targetPos = targetStyle.position;
+        const target = e.currentTarget.closest(`[${this.opts.selector}]`),
+            // target 위치 정보
+            {width: w, height: h, left, top} = target.getBoundingClientRect(),
+            // touch/mouse 위치 정보
+            touchX = e.clientX - left,
+            touchY = e.clientY - top,
+            // touch 위치로부터 가장 먼 모서리를 구함
+            targetX = (touchX > w / 2) ? 0 : w,
+            targetY = (touchY > h / 2) ? 0 : h,
+            // ripple의 지름을 구함
+            distance = Math.sqrt(Math.pow(Math.abs(targetX - touchX), 2) + Math.pow(Math.abs(targetY - touchY), 2)) * 2,
+            // style 저장
+            targetStyle = target.style,
+            targetPos = targetStyle.position;
 
         if (!targetPos || targetPos === 'static') {
             targetStyle.position = 'relative';
@@ -60,7 +66,8 @@ class Ripple {
         rippleWrap.classList.add(this.opts.rippleClassName);
 
         const ripple = document.createElement('span');
-        ripple.style.cssText = `background:${target.getAttribute(this.opts.selector)};width:${d}px;height:${d}px;left:${x - (d / 2)}px;top:${y - (d / 2)}px;`
+        // reflow 발생으로 style은 cssText를 이용
+        ripple.style.cssText = `background:${target.getAttribute(this.opts.selector)};width:${distance}px;height:${distance}px;left:${touchX - (distance / 2)}px;top:${touchY - (distance / 2)}px;`
         ripple.addEventListener(this.opts.animationEndEvent, this.rippleEnd.bind(this), false);
 
         rippleWrap.appendChild(ripple);
